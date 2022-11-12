975ebc12a66cddcbd8dbb6a25b41b5d6846c97d1
Ferran <ferran.obiols.colomina@gmail.com>
2022-11-12 06:20:36 -0500

    Created tools models and add migrations in gitignore

--- toolsofpower/tools/admin.py
+++ toolsofpower/tools/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.Tag)
+admin.site.register(models.Source)
+admin.site.register(models.Platform)
+admin.site.register(models.Language)
+admin.site.register(models.Tool)
--- toolsofpower/tools/models.py
+++ toolsofpower/tools/models.py
@@ -1,3 +1,43 @@
-from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+class Tag(models.Model):
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+class Source(models.Model):
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+class Platform(models.Model):
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+class Language(models.Model):
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+class Tool(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=150)
+    description = models.TextField(blank=True)
+    tags = models.ManyToManyField(Tag, blank=True)
+    source =models.ForeignKey(Source, on_delete=models.PROTECT, blank=True)
+    used = models.BooleanField(default=False)
+    rating = models.FloatField(default=0, validators=[MaxValueValidator(10), MinValueValidator(0)])
+    platform = models.ForeignKey(Platform, on_delete=models.PROTECT, blank=True)
+    language = models.ForeignKey(Language, on_delete=models.PROTECT, blank=True)
+    related = models.ManyToManyField('self', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
