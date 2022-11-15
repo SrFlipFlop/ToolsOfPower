d351f23322d6548778acd8f5fd61dcb3bc30dfd7
Ferran <ferran.obiols.colomina@gmail.com>
2022-11-15 16:42:10 +0100

    First version fo GitHub Topics parser and add new application in Django settings (TODO: store tools in the database model)

---
+++ toolsofpower/tools/scripts/github_topic_parser.py
@@ -0,0 +1,55 @@
+from json import dump
+from time import sleep
+from random import randint
+from requests import get
+from bs4 import BeautifulSoup
+
+def has_pagination(html):
+    #if html.find('form', class_='ajax-pagination-form js-ajax-pagination'):
+    if html.find('input', {'name':'page'}):
+        return True
+    return False
+
+def request(url):
+    res = get(url)
+    if res.status_code != 200:
+        raise Exception(f'Error request - {res.url} | {res.status_code}')
+    print(f'[+] Requested {url} - {res.status_code} - {len(res.text)}')
+    return BeautifulSoup(res.text, features='html.parser')
+
+def get_articles(html):
+    articles = []
+    for article in html.find_all('article'):
+        try:
+            new_article = {
+                'name': article.find('h3').find_all('a')[-1].text.strip(),
+                'description': article.find('div', class_='color-bg-default rounded-bottom-2').find('div', class_='').get('text', ''),
+                'github_tags': [a.text.strip() for a in article.find('div', class_='color-bg-default rounded-bottom-2').find('div', class_='d-flex flex-wrap border-bottom color-border-muted px-3 pt-2 pb-2').find_all('a')],
+            }
+            articles.append(new_article)
+        except Exception as e:
+            print(f'[!] Error in get_articles - {e} - {new_article}')
+    return articles
+
+#TODO: recursive func
+def get_topics():
+    page = 1
+    html = request(f'https://github.com/topics/pentest?page={page}')
+    articles = get_articles(html)
+    
+    while has_pagination(html):
+        try:
+            page += 1
+            html = request(f'https://github.com/topics/pentest?page={page}')
+            articles += get_articles(html)
+            sleep(randint(1,5))
+        except Exception as e:
+            print(f'[!] Error in get_topics - {e}')
+            break
+    return articles
+
+def run():
+    topics = get_topics()
+    print(f'[+] Total tools found - {len(topics)}')
+    with open('/tmp/example.json', 'w') as f:
+        dump(topics, f)
